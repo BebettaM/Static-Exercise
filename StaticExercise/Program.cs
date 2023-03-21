f59a093c1db7d7838240d765bb88cb24a256d112
Bebetta Motondo <claramotondo2@yahoo.fr>
2023-03-21 14:21:03 -0500

    Static

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var celcius = TempConverter.FahrenheitToCelsius(68);
+            var fahrenheit = TempConverter.CelciusToFahrenheit(20);
+
+            Console.WriteLine("After conversion:");
+            Console.WriteLine($"celcius {celcius}");
+            Console.WriteLine($"fahrenheit {fahrenheit}");
+
         }
     }
 }
